0b7e427ef37c0d40877ce0d86c68eabdf9ca07a1
Dashkere404 <sabarkina@gmail.com>
2023-10-22 15:15:34 +0300

    dasha

---
+++ fdfd.py
@@ -0,0 +1,19 @@
+guess=('kakashka')
+leng=len(guess)
+slovo=("_ "*leng)
+print (slovo)
+slovo = list(slovo)
+while leng>0:
+    lett=str(input("Введите букву: "))
+    if lett in guess:
+        leng-=guess.count(lett)
+        print("Такая буква есть в слове!")
+        for i in range (len(guess)):
+            if guess[i]==lett:
+                slovo[i*2]=lett
+                print ("".join(slovo))
+    else:
+        print ("Masha")
+print ("Вы выйграли!!! Можете убить короля!!!)))))")
+
+    
